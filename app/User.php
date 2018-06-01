c5923604a5b1ddca5a4d70e2afedaa4f406067d9
Yevhenii Riabyi <zenia9012@gmail.com>
2018-06-01 21:21:44 +0300

    Fix error message in permision manager

--- app/User.php
+++ app/User.php
@@ -2,12 +2,16 @@
 
 namespace App;
 
+use Backpack\CRUD\CrudTrait;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use HasRoles;
+    use CrudTrait;
 
     /**
      * The attributes that are mass assignable.
